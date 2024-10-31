290582c1c0072d9285bbeae5e2e4e75f36f14c7d
wanyos <juanjoromero9@gmail.com>
2024-10-31 13:20:20 +0100

    new component top interactions

--- src/components/vue-data-ui-components/VueUiSparkStack.vue
+++ src/components/vue-data-ui-components/VueUiSparkStack.vue
@@ -1,55 +1,78 @@
 <template>
-    <div class="div__main">
-        <VueDataUi component="VueUiSparkStackbar" :dataset="dataset" :config="config" />
-    </div>
+  <div class="div__main">
+    <VueDataUi component="VueUiSparkStackbar" :dataset="dataset" :config="config" />
+  </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, reactive, computed } from 'vue'
 import { VueDataUi } from 'vue-data-ui'
 import 'vue-data-ui/style.css'
 
-const dataset = ref([
-  {
-    name: 'Interactions',
-    value: 258,
-    color: '#DC73FC'
+const props = defineProps({
+  data: {
+    type: Array,
+    default: () => []
   },
-  {
-    name: 'Sent messages',
-    value: 36,
-    color: '#08B545'
+  address: {
+    type: Object,
+    default: () => {}
   },
-  {
-    name: 'Received messages',
-    value: 16,
-    color: '#0096FB'
+  showLegend: {
+    type: Boolean,
+    default: true
   }
-])
+})
 
-const config = ref({
+const dataset = ref(props.data)
+
+// const dataset = ref([
+//   {
+//     name: 'Interactions',
+//     value: 258,
+//     color: '#DC73FC'
+//   },
+//   {
+//     name: 'Sent messages',
+//     value: 36,
+//     color: '#08B545'
+//   },
+//   {
+//     name: 'Received messages',
+//     value: 16,
+//     color: '#0096FB'
+//   }
+// ])
+
+const config = reactive({
   style: {
     backgroundColor: '#fff',
     fontFamily: 'inherit',
     animation: { show: true, animationFrames: 60 },
     bar: { gradient: { show: true, intensity: 40, underlayerColor: '#fff' } },
     legend: {
-      margin: '6px 0 0 0',
+      margin: '1px 0 0 0',
       textAlign: 'left',
-      show: false,
+      show: computed(() => !props.showLegend),
       fontSize: 10,
       name: { color: '#1a1a1a', bold: false },
       value: { show: true, bold: false, color: '#1a1a1a', prefix: '', suffix: '', rounding: 0 },
       percentage: { show: true, bold: true, color: '#1a1a1a', rounding: 1 }
     },
     title: {
+      show: true,
       textAlign: 'left',
-      text: '',
-      color: '#FAFAFA',
+      text: computed(() => props.address?.name || 'empty'),
+      color: '#1a1a1a',
       fontSize: 16,
       bold: true,
-      margin: '0 0 6px 0',
-      subtitle: { color: '#A1A1A1', text: '', fontSize: 12, bold: false }
+      margin: '0 0 2px 0',
+      subtitle: {
+        color: '#A1A1A1',
+        text: computed(() => props.address?.email || 'empty'),
+        fontSize: 10,
+        bold: false
+      }
     }
   }
 })
@@ -57,6 +80,7 @@ const config = ref({
 
 <style scoped>
 .div__main {
-    width: 35em;
+  margin-bottom: 10px;
+  width: 35em;
 }
 </style>
--- src/views/vuedataui-view/VueUiSparkStack.vue
+++ src/views/vuedataui-view/VueUiSparkStack.vue
@@ -1,32 +1,183 @@
 <template>
-    <div>
-      <h3>VueUiSparkStack</h3>
-  
-      <section class="section__quick">
-      <VueUiSparkStack />
-      </section>
-  
-    
+  <div>
+    <h3>VueUiSparkStack</h3>
+
+    <button @click="showLegend = !showLegend">Change</button>
+    <div class="div__legend">
+      <div v-if="showLegend" class="div__container">
+        <div class="div__item">
+          <div class="div__icon" style="background-color: #dc73fc"></div>
+          <label for="">Interactions</label>
+        </div>
+
+        <div class="div__item">
+          <div class="div__icon" style="background-color: #08b545"></div>
+          <label for="">Sent messages</label>
+        </div>
+
+        <div class="div__item">
+          <div class="div__icon" style="background-color: #0096fb"></div>
+          <label for="">Received messages</label>
+        </div>
+      </div>
     </div>
-  </template>
-  
-  <script setup>
-import VueUiSparkStack from '@/components/vue-data-ui-components/VueUiSparkStack.vue';
-  
-  // const colorChartSent = '#08B545'
-  // const colorChartReceived = '#0096FB'
-  
- 
-  </script>
-  
-  <style lang="css" scoped>
-  section {
-    margin-top: 15px;
-    display: flex;
-    justify-content: center;
+
+    <section class="section__quick">
+      <VueUiSparkStack :data="dataValues1" :address="dataName1" :show-legend="showLegend" />
+      <VueUiSparkStack :data="dataValues2" :address="dataName2" :show-legend="showLegend" />
+      <VueUiSparkStack :data="dataValues3" :address="dataName3" :show-legend="showLegend" />
+      <VueUiSparkStack :data="dataValues4" :address="dataName4" :show-legend="showLegend" />
+    </section>
+  </div>
+</template>
+
+<script setup>
+import VueUiSparkStack from '@/components/vue-data-ui-components/VueUiSparkStack.vue'
+import { ref } from 'vue'
+
+const showLegend = ref(true)
+
+const dataName1 = {
+  name: 'Wanyos',
+  email: 'wanyos99@emailmeter.com'
+}
+
+const dataName2 = {
+  name: 'Control Laboral',
+  email: 'no-reply@controllaboral.es'
+}
+
+const dataName3 = {
+  name: 'Ryan',
+  email: 'ryan@shuttlecloud.com'
+}
+
+const dataName4 = {
+  name: 'Carlos Perez Gandara',
+  email: 'c@shuttlecloud.com'
+}
+
+const dataValues1 = [
+  {
+    name: 'Interactions',
+    value: 25,
+    color: '#DC73FC'
+  },
+  {
+    name: 'Sent messages',
+    value: 36,
+    color: '#08B545'
+  },
+  {
+    name: 'Received messages',
+    value: 16,
+    color: '#0096FB'
   }
-  
-  section .item {
-    margin: 5px 55px;
+]
+const dataValues2 = [
+  {
+    name: 'Interactions',
+    value: 5,
+    color: '#DC73FC'
+  },
+  {
+    name: 'Sent messages',
+    value: 16,
+    color: '#08B545'
+  },
+  {
+    name: 'Received messages',
+    value: 36,
+    color: '#0096FB'
   }
-  </style>
+]
+const dataValues3 = [
+  {
+    name: 'Interactions',
+    value: 15,
+    color: '#DC73FC'
+  },
+  {
+    name: 'Sent messages',
+    value: 6,
+    color: '#08B545'
+  },
+  {
+    name: 'Received messages',
+    value: 32,
+    color: '#0096FB'
+  }
+]
+const dataValues4 = [
+  {
+    name: 'Interactions',
+    value: 34,
+    color: '#DC73FC'
+  },
+  {
+    name: 'Sent messages',
+    value: 3,
+    color: '#08B545'
+  },
+  {
+    name: 'Received messages',
+    value: 6,
+    color: '#0096FB'
+  }
+]
+
+// const colorChartSent = '#08B545'
+// const colorChartReceived = '#0096FB'
+</script>
+
+<style lang="css" scoped>
+section {
+  border: 1px solid black;
+  margin: 0 auto;
+  width: 55em;
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+}
+
+section .item {
+  margin: 5px 55px;
+}
+
+.div__legend {
+  display: flex;
+  justify-content: space-around;
+  padding: 10px;
+}
+
+.div__icon {
+  width: 10px;
+  height: 10px;
+  border: 1px solid black;
+  border-radius: 50%;
+  margin-right: 5px;
+}
+
+.div__container {
+  width: 450px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.div__item {
+  display: flex;
+  align-items: center;
+}
+
+label {
+  font-size: 14px;
+  color: #1a1a1a;
+}
+
+button {
+  padding: 2px 10px;
+  border-radius: 6px;
+  cursor: pointer;
+}
+</style>
